988c2f5db22fc16318065fe672af69c4bd228a1c
ainwg <87463461+ainwg@users.noreply.github.com>
2026-08-17 13:20:35 +0800

    Extract multiple web elements in a page

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -9,8 +9,14 @@ import { test, expect } from "@playwright/test";
 
 test("Page Playwright Test", async ({ page }) => {
   await page.goto("https://www.saucedemo.com");
-  await page.locator("#user-name").fill("user");
+  await page.locator("#user-name").fill("standard_user");
   await page.locator("#password").fill("secret_sauce");
   await page.locator("#login-button").click();
-  console.log(await page.locator("h3[data-test='error']").textContent());
+  //console.log(await page.locator("h3[data-test='error']").textContent());
+  console.log(
+    await page.locator("div .inventory_item_name").first().textContent(),
+  );
+  console.log(
+    await page.locator("div .inventory_item_name").nth(1).textContent(),
+  );
 });
