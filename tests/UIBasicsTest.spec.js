3e03f69e2a3d8c2565cd67c061927c7039282e67
ainwg <87463461+ainwg@users.noreply.github.com>
2026-08-17 13:04:53 +0800

    Test for incorrect input

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -9,7 +9,8 @@ import { test, expect } from "@playwright/test";
 
 test("Page Playwright Test", async ({ page }) => {
   await page.goto("https://www.saucedemo.com");
-  await page.locator("#user-name").fill("standard_user");
+  await page.locator("#user-name").fill("user");
   await page.locator("#password").fill("secret_sauce");
-  await page.locator("#login-button").click;
+  await page.locator("#login-button").click();
+  console.log(await page.locator("h3[data-test='error']").textContent());
 });
