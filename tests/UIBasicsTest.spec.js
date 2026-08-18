ce467b4842eef05191f0cbb492c690d3a9a26566
ainwg <87463461+ainwg@users.noreply.github.com>
2026-08-18 22:15:26 +0800

    Handle select dropdown and checkbox

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -13,10 +13,27 @@ test("Page Playwright Test", async ({ page }) => {
   await page.locator("#password").fill("secret_sauce");
   await page.locator("#login-button").click();
   //console.log(await page.locator("h3[data-test='error']").textContent());
-  console.log(
-    await page.locator("div .inventory_item_name").first().textContent(),
-  );
-  console.log(
-    await page.locator("div .inventory_item_name").nth(1).textContent(),
-  );
+  //console.log(await page.locator("div .inventory_item_name").first().textContent());
+  //console.log(await page.locator("div .inventory_item_name").nth(1).textContent(),);
+  console.log(await page.locator("div .inventory_item_name").allTextContents());
+});
+
+test.only("UI Controls", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  const username = page.locator("#username");
+  const password = page.locator("#password");
+  const signInButton = page.locator("#signInBtn");
+  const dropdown = page.locator("select.form-control");
+  const radiobtn = page.locator(".radiotextsty");
+  const okButton = page.locator("#okayBtn");
+  const terms = page.locator("#terms");
+  // select value from dropdown
+  await dropdown.selectOption("stud");
+  // select value from radio button
+  await radiobtn.last().click();
+  await okButton.click();
+  console.log(await radiobtn.last().isChecked());
+  await expect(radiobtn.last()).toBeChecked();
+  await terms.click();
+  await expect(terms).toBeChecked();
 });
